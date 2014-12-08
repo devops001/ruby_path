c9ecd5455453320dfdd949987b4861f9b881f065
devops001 <devops001@gmail.com>
2014-12-08 10:44:47 -0800

    started TestCard & card

---
+++ test_card.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+require 'minitest/autorun'
+require_relative 'card'
+
+class TestCard < Minitest::Test
+
+  def setup
+    @card = Card.new("Test Card", "this is a test card")
+  end
+
+  def test_name
+    assert_equal("Test Card", @card.name)
+  end
+
+  def test_desc
+    assert_equal("this is a test card", @card.desc)
+  end
+
+end
+
